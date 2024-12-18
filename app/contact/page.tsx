0098092d56be04ed7f5e13bce5e656ab78b63208
Muritala Ahmed <muritalaahmed405@gmail.com>
2024-12-18 22:38:03 +0100

    webshot

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -87,7 +87,7 @@ export default function Contact() {
               onChange={handleChange}
               required
               rows={4}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 text-black border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             ></textarea>
           </div>
           <motion.button
